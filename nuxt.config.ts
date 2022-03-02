37f82021986c5aeacf7a79949ba8322074253e23
Jervie Yeh <jervie@thetwins.co>
2022-03-02 16:52:38 +0800

    import google font "Rubik"

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+    meta: {
+        link: [
+            { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+            { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap' },
+          ],
+    },
     css: [
         'normalize.css/normalize.css',
         '@/assets/css/main.scss'
