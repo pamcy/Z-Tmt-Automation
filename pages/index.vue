784446bb136ab3c9bc5b7237aa498292df5515da
Jervie Yeh <jervie@thetwins.co>
2022-03-08 13:53:36 +0800

    structure and style homepage hero section

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
       <Header />
+      <div class="hero">
+        <div class="container">
+          <div class="hero-text">
+            <h1>
+              <span class="text-uppercase">Professional<br aria-hidden="true"> grade quality</span><br aria-hidden="true">
+              <small>at a DIY-grade price</small>
+            </h1>
+            <p>With over seven million actuators produced annually, TMT Automation is a global leader in the field of linear actuators.We vows to bring to the market quality at an affordable price.</p>
+            <NuxtLink to="/" class="btn">
+              Start Automating
+              <svg width="39" height="13" viewBox="0 0 39 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 6.552h33.618M31.6 11.375l5.6-4.804-5.6-4.946" stroke="#EE7B45" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
