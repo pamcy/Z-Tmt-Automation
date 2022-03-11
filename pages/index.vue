7aa68b62a389e97dd1e7d73733cc258237ffd56e
Jervie Yeh <jervie@thetwins.co>
2022-03-11 18:21:24 +0800

    structure and style homepage product showcase section

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
               <small>at a DIY-grade price</small>
             </h1>
             <p>With over seven million actuators produced annually, TMT Automation is a global leader in the field of linear actuators.We vows to bring to the market quality at an affordable price.</p>
-            <NuxtLink to="/" class="btn">
+            <NuxtLink to="/" class="btn btn-lg">
               Start Automating
               <svg width="39" height="13" viewBox="0 0 39 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 6.552h33.618M31.6 11.375l5.6-4.804-5.6-4.946" stroke="#EE7B45" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>
             </NuxtLink>
@@ -41,17 +41,77 @@
           </div>
         </div>
       </div>
-      <div class="scenario" style="height: 800px"></div>
+      <div class="showcase">
+        <div class="showcase-situation d-none d-md-block"></div>
+        <div class="showcase-product">
+          <swiper :modules="swiperModules" :pagination="{ clickable: true }" autoplay loop>
+            <swiper-slide>
+              <div class="showcase-product-meta">
+                <h3 class="showcase-product-meta__title">Terrier</h3>
+                <span class="showcase-product-meta__spec">150/200</span>
+              </div>
+              <picture>
+                <source srcset="/assets/images/product-1-mobile.jpg" media="(max-width: 1199px)" />
+                <img src="/assets/images/product-1-desktop.jpg" alt>
+              </picture>
+              <NuxtLink to="/" class="btn btn--with-border">
+                View Products
+                <svg width="39" height="13" viewBox="0 0 39 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 6.552h33.618M31.6 11.375l5.6-4.804-5.6-4.946" stroke="#EE7B45" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>
+              </NuxtLink>
+            </swiper-slide>
+            <swiper-slide>
+              <div class="showcase-product-meta">
+                <h3 class="showcase-product-meta__title">Mastiff</h3>
+                <span class="showcase-product-meta__spec">300/400/400L</span>
+              </div>
+              <picture>
+                <source srcset="/assets/images/product-2-mobile.jpg" media="(max-width: 1199px)" />
+                <img src="/assets/images/product-2-desktop.jpg" alt>
+              </picture>
+              <NuxtLink to="/" class="btn btn--with-border">
+                View Products
+                <svg width="39" height="13" viewBox="0 0 39 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 6.552h33.618M31.6 11.375l5.6-4.804-5.6-4.946" stroke="#EE7B45" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>
+              </NuxtLink>
+            </swiper-slide>
+            <swiper-slide>
+              <div class="showcase-product-meta">
+                <h3 class="showcase-product-meta__title">Mastiffls</h3>
+                <span class="showcase-product-meta__spec">300LS/400LS/400LLS</span>
+              </div>
+              <picture>
+                <source srcset="/assets/images/product-3-mobile.jpg" media="(max-width: 1199px)" />
+                <img src="/assets/images/product-3-desktop.jpg" alt>
+              </picture>
+              <NuxtLink to="/" class="btn btn--with-border">
+                View Products
+                <svg width="39" height="13" viewBox="0 0 39 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 6.552h33.618M31.6 11.375l5.6-4.804-5.6-4.946" stroke="#EE7B45" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>
+              </NuxtLink>
+            </swiper-slide>
+          </swiper>
+        </div>
+      </div>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 
+import { Navigation, Pagination, Autoplay, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 const heroSection = ref(null)
 const possibilitySection = ref(null)
 const video = ref(null)
 const videoActivated = ref(false)
+
+const swiperModules = [
+  Navigation,
+  Pagination,
+  Autoplay,
+  A11y
+]
 
 onMounted(() => {
   setDocHeight()
