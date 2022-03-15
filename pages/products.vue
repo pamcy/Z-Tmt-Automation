c5c13332101eda311a1a507b6f8a3ea7f9c171fd
Chinyi <chinyi.pam@gmail.com>
2022-03-15 14:31:13 +0800

    add page header

---
+++ pages/products.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Header />
+
+    <div class="page-header">
+      <div class="container">
+        <div class="page-header-text">
+          <h1>Products</h1>
+          <p>TMT offers a wide range of products, specializing in swing gates, sliding gates and garage doors.</p>
+          <p>Worldwide, we have distributors in over 50 countries and sell over 1 million units per annum.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+</script>
+
