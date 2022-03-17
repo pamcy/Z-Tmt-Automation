db6728c39eb426b10e5398cac00c8a6c1eae1110
Jervie Yeh <jervie@thetwins.co>
2022-03-17 16:39:15 +0800

    structure support page's header and breadcrumb

---
+++ pages/support.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="support">
+        <div class="page-header page-header--sm" ref="pageHeaderSection">
+            <div class="container">
+                <div class="page-header-text">
+                    <h1>Support</h1>
+                    <p>Welcome to TMT Support! Custom-made services tailored for you.</p>
+                </div>
+            </div>
+        </div>
+        <main>
+            <div class="container-sm">
+                <nav class="breadcrumb">
+                    <ul>
+                        <li>
+                            <NuxtLink to="/">Home</NuxtLink>
+                        </li>
+                        <li class="is-active">
+                            <span>Support</span>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </main>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const pageHeaderSection = ref(null)
+
+onMounted(() => {
+  pageHeaderSection.value.classList.add('is-loaded')
+})
+
+</script>
