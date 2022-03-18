b5c5c32a2995b5526f64c69027e3b461ffcafb33
Jervie Yeh <jervie@thetwins.co>
2022-03-18 16:24:30 +0800

    structure and style basic schedule a call page

---
+++ pages/schedule-a-call.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="schedule-a-call">
+        <div class="page-header page-header-schedule-a-call page-header--image-only"></div>
+        <main>
+            <div class="container-sm">
+                <nav class="breadcrumb">
+                    <ul>
+                        <li>
+                            <NuxtLink to="/">Home</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="/support">Support</NuxtLink>
+                        </li>
+                        <li class="is-active">
+                            <span>Schedule a Call</span>
+                        </li>
+                    </ul>
+                </nav>
+                 <h1 class="h2">Schedule a Call</h1>
+                <p>You can schedule a technical support call below. This will allow us to help you troubleshoot any problems that you may have. You will be contacted through your selected means at the allotted time. Please allow up to 10 minutes for the previous call to wrap up.</p>
+                
+            </div>
+        </main>
+    </div>
+</template>
+
+<script setup>
+
+</script>
