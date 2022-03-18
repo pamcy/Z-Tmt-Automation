556fffac2c7890179712c0f4c83bf1c6b1a0a163
Chinyi <chinyi.pam@gmail.com>
2022-03-18 10:45:50 +0800

    add page header

---
+++ pages/product-manuals.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="page-header page-header-product-manuals page-header--image-only"></div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+</script>
