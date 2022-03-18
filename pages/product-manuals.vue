d5592646ff89cf3a263d78faca9ebf83043ad1eb
Chinyi <chinyi.pam@gmail.com>
2022-03-18 15:14:08 +0800

    create product structure in manuals page

--- pages/product-manuals.vue
+++ pages/product-manuals.vue
@@ -21,7 +21,16 @@
           <button class="btn-styless" @click="onToggleSorting">
             <span v-if="currentSorting">{{ currentSorting }}</span>
             <span class="select-dropdown-placeholder" v-else>Type</span>
-            <svg width="18" height="12" viewBox="0 0 18 12" fill="none"><path d="m2 2 6.899 8L16 2" stroke="#EE7B45" stroke-width="2.7" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" /></svg>
+            <svg width="18" height="12" viewBox="0 0 18 12" fill="none">
+              <path
+                d="m2 2 6.899 8L16 2"
+                stroke="#EE7B45"
+                stroke-width="2.7"
+                stroke-miterlimit="10"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
           </button>
           <ul class="select-dropdown-menu">
             <li v-for="menu in sortingMenu" :key="menu">
@@ -30,12 +39,95 @@
           </ul>
         </div>
       </div>
+      <div class="container">
+        <ul class="product-cards">
+          <li
+            v-for="(item, index) in items"
+            :key="item.title"
+            class="product-card"
+            :style="{ transitionDelay: index * 200 + 'ms' }"
+          >
+            <div class="product-card__image">
+              <img :src="item.img" />
+              <span class="product-card__tag">{{ item.type }}</span>
+            </div>
+            <div class="product-card__content">
+              <h2>{{ item.title }}</h2>
+              <ul>
+                <li v-for="(language, index) in item.languages" :key="language">
+                  {{ language }} {{ index !== item.languages.length - 1 ? '&#124;' : '' }}
+                </li>
+              </ul>
+            </div>
+            <NuxtLink :to="item.url" class="btn">Download</NuxtLink>
+          </li>
+        </ul>
+      </div>
     </main>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
+
+const items = ref([
+  {
+    title: 'ChowHUB-BT',
+    type: 'pdf',
+    languages: ['ch'],
+    img: '/images/manual-1-chowhub-bt.jpg',
+    url: '#',
+  },
+  {
+    title: 'ChowHUB',
+    type: 'pdf',
+    languages: ['en', 'cn', 'th', 'de'],
+    img: '/images/manual-2-chowhub.jpg',
+    url: '#',
+  },
+  {
+    title: 'ChowCAM-Bullet1',
+    type: 'pdf',
+    languages: ['en', 'cn'],
+    img: '/images/manual-3-chowcam-bullet-1.jpg',
+    url: '#',
+  },
+  {
+    title: 'ChowCAM-Dome1',
+    type: 'pdf',
+    languages: ['en'],
+    img: '/images/manual-4-chowcam-dom1.jpg',
+    url: '#',
+  },
+  {
+    title: 'ChowCAM-Dome2',
+    type: 'pdf',
+    languages: ['en'],
+    img: '/images/manual-5-chowcam-dom2.jpg',
+    url: '#',
+  },
+  {
+    title: 'ChowCAM',
+    type: 'pdf',
+    languages: ['en', 'th'],
+    img: '/images/manual-6-chowcam.jpg',
+    url: '#',
+  },
+  {
+    title: 'ChowBELL-PRO',
+    type: 'pdf',
+    languages: ['en', 'cn'],
+    img: '/images/manual-7-chowbell-pro.jpg',
+    url: '#',
+  },
+  {
+    title: 'ChowBELL',
+    type: 'pdf',
+    languages: ['en', 'esp', 'cn', 'th', 'de'],
+    img: '/images/manual-8-chowbell.jpg',
+    url: '#',
+  },
+])
 
 const sortingIsOpen = ref(false)
 const sortingMenu = ref(['Type 1', 'Type 2'])
