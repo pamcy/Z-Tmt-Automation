567d3e8f93d4cc45b563b2a5c12ef55a96e93957
Chinyi <chinyi.pam@gmail.com>
2022-03-21 09:20:11 +0800

    add page header to warranty page

---
+++ pages/warranty.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="warranty">
+    <div class="page-header page-header-warranty page-header--image-only"></div>
+    <main>
+      <div class="container-sm">
+        <nav class="breadcrumb">
+          <ul>
+            <li>
+              <NuxtLink to="/">Home</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/support">Support</NuxtLink>
+            </li>
+            <li class="is-active">
+              <span>Warranty Info</span>
+            </li>
+          </ul>
+        </nav>
+
+        <h2>Warranty Info</h2>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+onMounted(() => {})
+</script>
