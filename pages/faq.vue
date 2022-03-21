b2ccae63de70fd4a35451f60dae6d22e4b725451
Chinyi <chinyi.pam@gmail.com>
2022-03-21 16:51:29 +0800

    add page header to faq page

---
+++ pages/faq.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="faq">
+        <div class="page-header page-header-faq page-header--image-only"></div>
+        <main>
+            <div class="container-sm">
+                <nav class="breadcrumb">
+                    <ul>
+                        <li>
+                            <NuxtLink to="/">Home</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="/support">Support</NuxtLink>
+                        </li>
+                        <li class="is-active">
+                            <span>FAQ</span>
+                        </li>
+                    </ul>
+                </nav>
+
+                <h1 class="h2">Frequently Asked Question</h1>
+            </div>
+        </main>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {})
+
+onUnmounted(() => {})
+</script>
