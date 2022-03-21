9170b315f560aa0ae19f49ee72bff563d7c2b909
Chinyi <chinyi.pam@gmail.com>
2022-03-21 10:24:35 +0800

    structure & style the warranty content

--- pages/warranty.vue
+++ pages/warranty.vue
@@ -1,26 +1,64 @@
 <template>
-  <div class="warranty">
-    <div class="page-header page-header-warranty page-header--image-only"></div>
-    <main>
-      <div class="container-sm">
-        <nav class="breadcrumb">
-          <ul>
-            <li>
-              <NuxtLink to="/">Home</NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/support">Support</NuxtLink>
-            </li>
-            <li class="is-active">
-              <span>Warranty Info</span>
-            </li>
-          </ul>
-        </nav>
+    <div class="warranty">
+        <div class="page-header page-header-warranty page-header--image-only"></div>
+        <main>
+            <div class="container-sm">
+                <nav class="breadcrumb">
+                    <ul>
+                        <li>
+                            <NuxtLink to="/">Home</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="/support">Support</NuxtLink>
+                        </li>
+                        <li class="is-active">
+                            <span>Warranty Info</span>
+                        </li>
+                    </ul>
+                </nav>
 
-        <h2>Warranty Info</h2>
-      </div>
-    </main>
-  </div>
+                <h2>Warranty Info</h2>
+
+                <div class="warranty-content">
+                    <div class="warranty-content-intro">
+                        <b>TMT Automations will honor the obligations set in the warranty, with terms and conditions set forth below:</b>
+                        <p>TMT Automations wicll cover charges resulting from defects in workmanship and material. TMT will, at its discretion, provide repairs and direct replacement for all needed parts.</p>
+                    </div>
+                    <ul class="warranty-list">
+                        <li>
+                            <h3 class="section-title">Product Registration</h3>
+                            <p>We strong urge that the products be registered through the TMT Chow! App or through our website. New products or replacement parts should be registered within 90 days of purchase. If the product is not registered, then the warranty will be in effect starting from the date of purchase.</p>
+                        </li>
+                        <li>
+                            <h3 class="section-title">How to File for a Claim</h3>
+                            <p>In order to file for a claim under the warranty, you must:</p>
+                            <p>We reserve the right to oblige by the warranty if you cannot provide the proof of purchase as specified above. To file a for a claim, you can contact our customer fill out our form online at <a href="https://www.tmtautomation.com">www.tmtautomation.com</a>, email customer support at <a href="mailto:support@tmtautomation.com">support@tmtautomation.com</a> or call our support line at a 000-000-0000. Provide your product serial number and a copy of the receipt as well as the problem that you are facing. We will investigate the problem and promptly send you replacement parts if necessary.</p>
+                            <p>Before attempting to mailing in any defective parts, make sure that you have contacted customer support and filed for a claim. You will be given a Return Merchandise Authorization (RMA) number, as well as further instructions on the next steps. After receiving a RMA number, it should be clearly labelled on the outside of the return packaging.</p>
+                        </li>
+                        <li>
+                            <h3 class="section-title">What this Warranty Covers</h3>
+                            <p>To qualify for this warranty, your TMT product must be purchased from an authorized dealer or channel.</p>
+                            <p>TMT Automation will cover any defects in material or workmanship of our products within the defined warranty term, provided proper installation and use have been followed. The manufacturer will, at its discretion, provide repair and replacements at no cost to the customer within the warranty term. TMT Automations will pay for shipping fees, equaling to United Parcel Service ground fees.</p>
+                            <p>This limited warranty will NOT cover any damages or malfunctions from alterations, modifications, improper or unreasonable use or maintenance of our products and lightning, fire, power surges and other acts of nature.</p>
+                            <p>To avoid voiding the warranty, do not make any unauthorized modifications and repairs..</p>
+                        </li>
+                        <li>
+                            <h3 class="section-title">How Long Does the Warranty Last</h3>
+                            <p>The warranty for the different parts is as defined below:</p>
+                        </li>
+                        <li>
+                            <h3 class="section-title">Ways to Extend Warranty</h3>
+                            <p>You can extend your warranty through purchasing the extension options available on our website. To extend your warranty, you must purchase the extension before your original warranty expires.</p>
+                        </li>
+                        <li>
+                            <h3 class="section-title">Limitations on Liability</h3>
+                            <p>The maximum liability for any TMT Automations product under this limited liability will not exceed the actual purchase price of the product.</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+    </div>
 </template>
 
 <script setup>
