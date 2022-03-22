9f2de77c72e3fa9d8a141331d6bb2c617b48e991
Chinyi <chinyi.pam@gmail.com>
2022-03-22 11:58:15 +0800

    allow to open only one accordion at a time

--- components/OneOpenAccordion.vue
+++ components/OneOpenAccordion.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="one-open-accordion" :class="{ 'is-expanded': isExpanded }">
         <h3 class="accordion-title">
-            <button class="btn btn-styless" ref="accordionToggle" :aria-expanded="isExpanded" @click="toggleAccordion()">
-                <span><slot name="index" /></span>
+            <button class="btn btn-styless" :aria-expanded="isExpanded">
+                <span>{{ index + 1 }}</span>
                 <span><slot name="title" /></span>
                 <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path fill="#EE7B45" d="M0 8.003h20v4H0z" />
@@ -20,14 +20,5 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-
-const isExpanded = ref(false)
-const accordionToggle = ref(null)
-
-onMounted(() => {})
-
-function toggleAccordion() {
-    isExpanded.value = !isExpanded.value
-}
+const { index, isExpanded } = defineProps(['index', 'isExpanded'])
 </script>
