6d0d6339b9d6fc95904057690b54889671429e60
Chinyi <chinyi.pam@gmail.com>
2022-03-23 09:18:49 +0800

    add page header to installation page

---
+++ pages/installation-videos.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="installation">
+        <div class="page-header page-header-installation page-header--image-only"></div>
+        <main>
+            <div class="container-sm">
+                <nav class="breadcrumb">
+                    <ul>
+                        <li>
+                            <NuxtLink to="/">Home</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="/support">Support</NuxtLink>
+                        </li>
+                        <li class="is-active">
+                            <span>Installation Videos</span>
+                        </li>
+                    </ul>
+                </nav>
+
+                <h1 class="h2">Installation Videos</h1>
+            </div>
+        </main>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {})
+
+onUnmounted(() => {})
+</script>
