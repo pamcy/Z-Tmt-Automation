365e155f41e3e9fe8c07d40645193ebb426380b3
Chinyi <chinyi.pam@gmail.com>
2022-03-24 16:53:43 +0800

    add page header to search page

---
+++ pages/search.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="search">
+        <div class="page-header page-header-search is-loaded">
+            <div class="container-sm">
+                <div class="page-header-text">
+                    <h1>Search</h1>
+                </div>
+            </div>
+        </div>
+        <main>
+            <div class="container-sm">
+                <nav class="breadcrumb">
+                    <ul>
+                        <li>
+                            <NuxtLink to="/">Home</NuxtLink>
+                        </li>
+                        <li class="is-active">
+                            <span>Search</span>
+                        </li>
+                    </ul>
+                </nav>
+
+                <h2>Search Result</h2>
+
+                <SearchBox :searchValue="searchValue" />
+            </div>
+        </main>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import SearchBox from '../components/SearchBox.vue'
+
+const searchValue = ref('gate')
+
+onMounted(() => {})
+
+onUnmounted(() => {})
+</script>
