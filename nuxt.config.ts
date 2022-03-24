ea6bc8225ce210fdca6c162c0be41ac75934e008
Shih-You Yeh <yesyo@thetwins.co>
2022-03-24 17:14:37 +0800

    try to fix ESM issues caused by v-calendar
    https://v3.nuxtjs.org/concepts/esm/#troubleshooting-esm-issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+    build: {
+        transpile: ['v-calendar']
+    },
     meta: {
         link: [
             { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
