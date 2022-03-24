e367f22aa2b2597b2c134b66d9aad73f511da0e7
Chinyi <chinyi.pam@gmail.com>
2022-03-24 11:41:57 +0800

    structure and style info-card in about page

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -28,6 +28,31 @@
                     <strong>To provide the best smart solution for the users.</strong>
                     <p>TMT started as a small company that manufactured motors and actuator for some of the biggest names in the industry. Over the past decades, over 100 million units of our products have been sold. Throughout the years, our manufacturing and design skills have been honed to surpass the international standards and become a leader in the gate automation industry. However, our brand remained relatively unknown to the public as our products were branded through the bigger names. Now, TMT has decided that we want our products to be available to the public, no longer satisfied with just manufacturing solutions for other brands.</p>
                     <p>Producing over seven million units per year, TMT has its own in-house development and production teams. With over seven production facilities around the world, TMT’s can guarantee that you have the best products. Each item is rigorously inspected and tested; our quality assurance will exceed even the most scrupulous of standards. Our refined products serve to bring you, our esteemed customers, the best experience possible. Just like us, you have worked hard to be where you are, and that’s why you deserve the very best.</p>
+
+                    <ul class="info-cards">
+                        <li class="card card-data">
+                            <em>90% +</em>
+                            <span>Key Components<br /> made in-house</span>
+                        </li>
+                        <li class="card card-data">
+                            <em>2005</em>
+                            <span>Founded in<br /> Taiwan</span>
+                        </li>
+                        <li class="card card-data">
+                            <em>2,000 +</em>
+                            <span>Employees for<br /> manufacture</span>
+                        </li>
+                        <li class="card card-data">
+                            <em>14 +</em>
+                            <span>Over 14 Subsidiary<br /> in our Organization</span>
+                        </li>
+                        <li class="card">
+                            <span>ISO 9001 Certified for Outstanding Quality Management</span>
+                        </li>
+                        <li class="card">
+                            <span>Products Trusted in Medical Applications</span>
+                        </li>
+                    </ul>
                 </div>
             </main>
         </div>
