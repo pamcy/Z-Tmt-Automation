b7483d736a59151bc93b92129e4b1e8e1b3936b5
Chinyi <chinyi.pam@gmail.com>
2022-03-25 11:57:51 +0800

    fade in comparison content

--- pages/features/comparison.vue
+++ pages/features/comparison.vue
@@ -9,7 +9,7 @@
             </div>
         </div>
 
-        <main ref="introSection">
+        <main ref="bodySection">
             <div class="container-sm">
                 <nav class="breadcrumb">
                     <ul>
@@ -26,9 +26,9 @@
                 </nav>
 
                 <h2>TMT Automation’s Advantage</h2>
-                <p class="">Don’t settle for plastic components and low output motors. TMT Automation uses the highest quality materials to ensure a build that can weather the most rugged of uses. Not only endeavors to deliver outstanding quality, but TMT also strives for environmentally sustainability.</p>
-                <p class="">Below is a table comparing the swing gate openers of the same classes offered by some of the other leading DIY gate opener brands. You will find that TMT offers quality beyond this class.<br />*Some information regarding the competitors are not available.</p>
-                <img src="/images/comparison-table.png" alt />
+                <p class="fade-in delay-1">Don’t settle for plastic components and low output motors. TMT Automation uses the highest quality materials to ensure a build that can weather the most rugged of uses. Not only endeavors to deliver outstanding quality, but TMT also strives for environmentally sustainability.</p>
+                <p class="fade-in delay-2">Below is a table comparing the swing gate openers of the same classes offered by some of the other leading DIY gate opener brands. You will find that TMT offers quality beyond this class. *Some information regarding the competitors are not available.</p>
+                <img class="fade-in delay-3" src="/images/comparison-table.png" alt />
             </div>
         </main>
     </div>
@@ -37,5 +37,26 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 
-onMounted(() => {})
+const pageHeaderSection = ref(null)
+const bodySection = ref(null)
+
+onMounted(() => {
+    setTimeout(() => {
+        if (pageHeaderSection.value) {
+            pageHeaderSection.value.classList.add('is-loaded')
+        }
+    }, 100)
+
+    window.addEventListener('scroll', fadeInContent)
+})
+
+onUnmounted(() => {
+    window.removeEventListener('scroll', fadeInContent)
+})
+
+function fadeInContent() {
+    if (window.innerHeight - bodySection.value.getBoundingClientRect().top >= bodySection.value.offsetHeight / 2) {
+        bodySection.value.classList.add('is-revealed')
+    }
+}
 </script>
