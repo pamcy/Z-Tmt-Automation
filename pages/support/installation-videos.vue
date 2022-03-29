382beea9d9735624056edad60f75d8716c9aa003
Chinyi <chinyi.pam@gmail.com>
2022-03-29 10:14:58 +0800

    adjust more btn on hover in installation-slider

--- pages/support/installation-videos.vue
+++ pages/support/installation-videos.vue
@@ -54,12 +54,12 @@
                                 <span class="installation-slider-title">How to connect Chow! x IFTTT (demo video)</span>
                             </swiper-slide>
                         </swiper>
-                        <div class="video-more-btn">
-                            <NuxtLink :to="youtubeChannel" class="btn btn--no-background" target="_blank">
-                                More
-                                <svg width="39" height="13" viewBox="0 0 39 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 6.552h33.618M31.6 11.375l5.6-4.804-5.6-4.946" stroke="#EE7B45" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" /></svg>
-                            </NuxtLink>
-                        </div>
+                    </div>
+                    <div class="video-more-btn">
+                        <NuxtLink :to="youtubeChannel" class="btn btn--no-background" target="_blank">
+                            More
+                            <svg width="39" height="13" viewBox="0 0 39 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 6.552h33.618M31.6 11.375l5.6-4.804-5.6-4.946" stroke="#EE7B45" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" /></svg>
+                        </NuxtLink>
                     </div>
                 </div>
             </div>
