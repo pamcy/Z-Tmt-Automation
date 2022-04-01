32544f5c037b7139a81a595e410d8697855c86b9
Chinyi <chinyi.pam@gmail.com>
2022-04-01 09:34:19 +0800

    set meta title tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,16 @@ export default defineNuxtConfig({
         transpile: ['v-calendar']
     },
     meta: {
+        title: 'TMT AUTOMATION INC.',
+        meta: [
+            {
+              hid: 'description',
+              name: 'description',
+              content: 'my website description'
+            }
+        ],
         link: [
+            { rel: 'icon', type: 'image/x-icon', href: ''},
             { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
             { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
             { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap' },
