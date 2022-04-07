3819d89bc2b4d293209d7e7bb3164b1c9842f7a9
Chinyi <chinyi.pam@gmail.com>
2022-04-07 10:56:33 +0800

    update verification code image for demo purposes

--- pages/contact/contact-us.vue
+++ pages/contact/contact-us.vue
@@ -81,8 +81,8 @@
                         <div class="form-row">
                             <div class="form-group fade-in delay-8">
                                 <label for="verification">Verification Code<span class="required">*</span></label>
-                                <div class="form-group-varification">
-                                    <img src="http://placehold.jp/150x50.png">
+                                <div class="form-group-verification">
+                                    <img src="/images/verification-placeholder.png">
                                     <input type="text" id="verification" placeholder="Code" required>
                                 </div>
                             </div>
