40ab4895d3b83afc5093b4f7e220a41269d04f31
Jervie Yeh <jervie@thetwins.co>
2022-04-11 16:57:45 +0800

    minor adjustment

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
             <span class="text-uppercase">Professional<br aria-hidden="true"> grade quality</span><br aria-hidden="true">
             <small>at a DIY-grade price</small>
           </h1>
-          <p>With over seven million actuators produced annually, TMT Automation is a global leader in the field of linear actuators.We vow to bring to the market quality at an affordable price.</p>
+          <p>With over seven million actuators produced annually, TMT Automation is a global leader in the field of linear actuators. We vow to bring to the market quality at an affordable price.</p>
           <NuxtLink to="/products" class="btn btn-lg">
             Start Automating
             <svg width="39" height="13" viewBox="0 0 39 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 6.552h33.618M31.6 11.375l5.6-4.804-5.6-4.946" stroke="#EE7B45" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>
