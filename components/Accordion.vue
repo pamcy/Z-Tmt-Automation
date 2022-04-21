16bd054b49ee75f8a5204ad0bd23e3f5a4d597a9
Jervie Yeh <jervie@thetwins.co>
2022-04-21 14:49:15 +0800

    minor enhancements

--- components/Accordion.vue
+++ components/Accordion.vue
@@ -3,7 +3,7 @@
     <h2 class="accordion-title">
         <button class="btn btn-styless" ref="accordionToggle" :aria-expanded="isExpanded" @click="toggleAccordion()">
             <span><slot name="title" /></span>
-            <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg" :class="{ 'rotate-180': isExpanded, 'rotate-0': !isExpanded }" aria-hidden="true"><path fill="#EE7B45" d="M0 8.003h20v4H0z"/><path fill="#EE7B45" d="M12 0v20H8V0z"/></svg>
+            <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path fill="#EE7B45" d="M0 8.003h20v4H0z"/><path fill="#EE7B45" d="M12 0v20H8V0z"/></svg>
         </button>
     </h2>
 
--- components/SelectDropdown.vue
+++ components/SelectDropdown.vue
@@ -3,7 +3,7 @@
     <button type="button" class="btn btn-styless" @click="toggleSelectDropdown" :disabled="noData">
         <slot name="icon" />
         <slot name="title" />
-        <svg width="18" height="12" viewBox="0 0 18 12" fill="none"><path d="m2 2 6.899 8L16 2" stroke="#EE7B45" stroke-width="2.7" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" /></svg>
+        <svg width="18" height="12" viewBox="0 0 18 12" fill="none" aria-hidden="true"><path d="m2 2 6.899 8L16 2" stroke="#EE7B45" stroke-width="2.7" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" /></svg>
     </button>
     <div class="select-dropdown-menu">
         <div class="select-dropdown-menu__inner">
